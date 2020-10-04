fd1a369a66774e860574a89e2c94d5d2996d5b6c
Grayson Wen <wenoptics@gmail.com>
2020-10-03 23:52:53 -0400

    fix(alg): make private members

--- algorithms/recommendation/pipeline.py
+++ algorithms/recommendation/pipeline.py
@@ -34,60 +34,61 @@ class PipelineBase:
 class Pipeline(PipelineBase):
 
     def __init__(self):
-        self.skill_emb = None
-        self.norm_skill_dimen = None
-        self.job_des = None
+        self._skill_emb = None
+        self._norm_skill_dimen = None
+        self._job_des = None
 
     def load(self):
         print('Data preparing...')
         time_start = time.time()
         with open(_path_embedded) as json_file:
-            self.skill_emb = json.load(json_file)
+            self._skill_emb = json.load(json_file)
 
         with open(_path_hoff_desc) as fp:
-            self.job_des = json.load(fp)
+            self._job_des = json.load(fp)
 
-        _skills = [i['skills'] for i in self.job_des]
+        _skills = [i['skills'] for i in self._job_des]
         # 计算所有工作的embedding
-        skill_dimen = [self.skill([j.lower() for j in i]) for i in _skills]
+        skill_dimen = [self._skill([j.lower() for j in i]) for i in _skills]
 
-        self.norm_skill_dimen = [normalize(i) for i in skill_dimen]
+        self._norm_skill_dimen = [normalize(i) for i in skill_dimen]
         print(f'... done in {time.time() - time_start:.3} seconds')
         return self
 
-    def skill(self, x):
+    def _skill(self, x):
         a = ' '.join(x)
         a = a.split(' ')
         b = []
         for i in a:
-            if i in self.skill_emb:
-                b.append(self.skill_emb[i])
+            if i in self._skill_emb:
+                b.append(self._skill_emb[i])
         if b == []:
             return [0] * 300
         else:
             return list(np.mean(b, axis=0))
 
-    def cv_from_pdf(self, pdf_file):
+    def cv_from_pdf(self, pdf_file) -> str:
         return pdfminer.high_level.extract_text(pdf_file)
 
-    def cv_from_text(self, txt_file):
+    def cv_from_text(self, txt_file) -> str:
         with open(txt_file) as f:
             text = f.read()
         return text
 
     def get_job_recommendations(self, cv_text: str, top=5) -> List[Dict]:
+        """Return jobs from CV string"""
         if not cv_text:
             raise ValueError('CV text not valid')
 
         text = cv_text.lower().split()
         print('Text len =', len(text))
-        my_skill = self.skill(list(set([i for i in text if i in self.skill_emb.keys()])))
-        extract_skills = set([i for i in text if i in self.skill_emb.keys()])  # 从简历里面提取出在技能字典的词
+        my_skill = self._skill(list(set([i for i in text if i in self._skill_emb.keys()])))
+        extract_skills = set([i for i in text if i in self._skill_emb.keys()])  # 从简历里面提取出在技能字典的词
         print('Extract skills:', extract_skills)
 
-        a = np.array(self.norm_skill_dimen).dot(np.array(normalize(my_skill)))  # 计算简历和所有技能的相似性
+        a = np.array(self._norm_skill_dimen).dot(np.array(normalize(my_skill)))  # 计算简历和所有技能的相似性
 
-        jobs = [self.job_des[i] for i, j in sorted(enumerate(a), key=lambda x: x[1], reverse=True)[:top]]
+        jobs = [self._job_des[i] for i, j in sorted(enumerate(a), key=lambda x: x[1], reverse=True)[:top]]
         return jobs
 
 
