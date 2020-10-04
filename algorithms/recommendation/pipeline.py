08df0cb9bd8fd0375541492694d299f6a9d203b9
Grayson Wen <wenoptics@gmail.com>
2020-10-03 23:50:33 -0400

    feat(alg): wrap functionalities

--- algorithms/recommendation/pipeline.py
+++ algorithms/recommendation/pipeline.py
@@ -2,7 +2,7 @@ import json
 import time
 from pathlib import Path
 from pprint import pprint
-from typing import Dict
+from typing import Dict, List
 
 import numpy as np
 import pdfminer.high_level
@@ -12,32 +12,6 @@ _path_data = _path_cur / 'data'
 _path_embedded = _path_data / 'skill_emb.json'
 _path_hoff_desc = _path_data / 'hubstaff_job_descriptions.json'
 
-print('Data preparing...')
-time_start = time.time()
-with open(_path_embedded) as json_file:
-    skill_emb = json.load(json_file)
-
-with open(_path_hoff_desc) as fp:
-    job_des = json.load(fp)
-
-skills = [i['skills'] for i in job_des]
-
-
-def skill(x):
-    a = ' '.join(x)
-    a = a.split(' ')
-    b = []
-    for i in a:
-        if i in skill_emb:
-            b.append(skill_emb[i])
-    if b == []:
-        return [0] * 300
-    else:
-        return list(np.mean(b, axis=0))
-
-
-skill_dimen = [skill([j.lower() for j in i]) for i in skills]  # 计算所有工作的embedding
-
 
 def normalize(v):
     norm = np.linalg.norm(v)
@@ -46,46 +20,89 @@ def normalize(v):
     return v / norm
 
 
-norm_skill_dimen = [normalize(i) for i in skill_dimen]
-print(f'... done in {time.time() - time_start:.3} seconds')
+class PipelineBase:
+    def cv_from_pdf(self, cv_path):
+        raise NotImplementedError()
+
+    def cv_from_text(self, cv_path):
+        raise NotImplementedError()
+
+    def get_job_recommendations(self, cv_text: str, top=5):
+        raise NotImplementedError()
 
 
-def cv_from_pdf(pdf_file):
-    return pdfminer.high_level.extract_text(pdf_file)
+class Pipeline(PipelineBase):
 
+    def __init__(self):
+        self.skill_emb = None
+        self.norm_skill_dimen = None
+        self.job_des = None
 
-def cv_from_text(txt_file):
-    with open(txt_file) as f:
-        text = f.read()
-    return text
+    def load(self):
+        print('Data preparing...')
+        time_start = time.time()
+        with open(_path_embedded) as json_file:
+            self.skill_emb = json.load(json_file)
 
+        with open(_path_hoff_desc) as fp:
+            self.job_des = json.load(fp)
 
-def get_job_recommendations(cv_text: str, top=5) -> Dict:
-    if not cv_text:
-        raise ValueError('CV text not valid')
+        _skills = [i['skills'] for i in self.job_des]
+        # 计算所有工作的embedding
+        skill_dimen = [self.skill([j.lower() for j in i]) for i in _skills]
 
-    text = cv_text.lower().split()
-    print('Text len =', len(text))
-    my_skill = skill(list(set([i for i in text if i in skill_emb.keys()])))
-    extract_skills = set([i for i in text if i in skill_emb.keys()])  # 从简历里面提取出在技能字典的词
-    print('Extract skills:', extract_skills)
+        self.norm_skill_dimen = [normalize(i) for i in skill_dimen]
+        print(f'... done in {time.time() - time_start:.3} seconds')
+        return self
 
-    a = np.array(norm_skill_dimen).dot(np.array(normalize(my_skill)))  # 计算简历和所有技能的相似性
+    def skill(self, x):
+        a = ' '.join(x)
+        a = a.split(' ')
+        b = []
+        for i in a:
+            if i in self.skill_emb:
+                b.append(self.skill_emb[i])
+        if b == []:
+            return [0] * 300
+        else:
+            return list(np.mean(b, axis=0))
 
-    jobs = [job_des[i] for i, j in sorted(enumerate(a), key=lambda x: x[1], reverse=True)[:top]]
-    return jobs
+    def cv_from_pdf(self, pdf_file):
+        return pdfminer.high_level.extract_text(pdf_file)
+
+    def cv_from_text(self, txt_file):
+        with open(txt_file) as f:
+            text = f.read()
+        return text
+
+    def get_job_recommendations(self, cv_text: str, top=5) -> List[Dict]:
+        if not cv_text:
+            raise ValueError('CV text not valid')
+
+        text = cv_text.lower().split()
+        print('Text len =', len(text))
+        my_skill = self.skill(list(set([i for i in text if i in self.skill_emb.keys()])))
+        extract_skills = set([i for i in text if i in self.skill_emb.keys()])  # 从简历里面提取出在技能字典的词
+        print('Extract skills:', extract_skills)
+
+        a = np.array(self.norm_skill_dimen).dot(np.array(normalize(my_skill)))  # 计算简历和所有技能的相似性
+
+        jobs = [self.job_des[i] for i, j in sorted(enumerate(a), key=lambda x: x[1], reverse=True)[:top]]
+        return jobs
 
 
 if __name__ == '__main__':
     # Some quick and dirty tests
-    cv = cv_from_text('research/CV1.txt')
-    job_recommendations = get_job_recommendations(cv)
+    pipe = Pipeline().load()
+
+    cv = pipe.cv_from_text('research/CV1.txt')
+    job_recommendations = pipe.get_job_recommendations(cv)
     with open('research/Recom.json', 'w') as f:
         json.dump(job_recommendations, f)
     pprint(job_recommendations)
 
-    cv = cv_from_pdf('research/CV.pdf')
-    job_recommendations = get_job_recommendations(cv)
+    cv = pipe.cv_from_pdf('research/CV.pdf')
+    job_recommendations = pipe.get_job_recommendations(cv)
     with open('research/Recom_pdf.json', 'w') as f:
         json.dump(job_recommendations, f)
     pprint(job_recommendations)
