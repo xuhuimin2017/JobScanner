d582ac020af517a1714a6a0aae5ceeebc26bd25c
Grayson Wen <wenoptics@163.com>
2020-11-16 20:10:07 -0500

    feat: drag-n-drop skill editor

--- frontend/src/components/ListingView.vue
+++ frontend/src/components/ListingView.vue
@@ -59,30 +59,61 @@
       <q-item>
         <q-item-section>
           <q-item-label :lines="briefView ? 1 : 0">
-            <div class="row no-wrap q-gutter-lg">
+            <div class="row no-wrap q-gutter-lg skill-editor items-stretch" :class="{ skillEditing }">
               <div class="multiline-tag" :class="{ 'col-6': skillEditing }">
-                <draggable
-                  v-model="skillSelected"
-                  v-bind="dragOptions"
-                  @end="onDragEnd"
+                <drop-list
+                  :items="skillSelected"
+                  mode="cut"
+                  @insert="onInsertSkill(skillSelected, $event)"
+                  class="draggable-container full-height"
+                  @reorder="$event.apply(skillSelected)"
                 >
-                  <transition-group type="transition">
-                  <q-badge class="skill-tag q-mr-xs" text-color="white" v-for="s in skillSelected" :key="s"
-                           :color="mySkills.includes(s) ? 'primary' : 'orange'">
-                    {{ s }}
-                  </q-badge>
-                  </transition-group>
-                </draggable>
+                  <template v-slot:item="{item}">
+                    <drag class="draggable" :key="item" :data="item" @cut="removeSkill(skillSelected, item)">
+                      <q-badge
+                        class="skill-tag q-mr-xs"
+                        text-color="white"
+                        :color="mySkills.includes(item) ? 'primary' : 'orange'">{{ item }}</q-badge>
+                    </drag>
+                  </template>
+                  <template v-slot:feedback="{data}">
+                    <div class="draggable feedback" :key="data">
+                      <q-badge
+                        class="skill-tag q-mr-xs"
+                        text-color="white"
+                        outline
+                        :color="mySkills.includes(data) ? 'primary' : 'orange'">{{ data }}</q-badge>
+                    </div>
+                  </template>
+                </drop-list>
               </div>
+
               <div class="multiline-tag" v-if="skillEditing && !briefView">
-                <draggable v-model="mock.skillPool" v-bind="dragOptions" @end="onDragEnd">
-                  <transition-group type="transition">
-                  <q-badge class="skill-tag q-mr-xs" text-color="white" v-for="s in mock.skillPool" :key="s"
-                           :color="mySkills.includes(s) ? 'primary' : 'orange'">
-                    {{ s }}
-                  </q-badge>
-                  </transition-group>
-                </draggable>
+                <drop-list
+                  :items="mock.skillPool"
+                  mode="cut"
+                  class="draggable-container full-height"
+                  @insert="onInsertSkill(mock.skillPool, $event)"
+                  @reorder="$event.apply(mock.skillPool)"
+                >
+                  <template v-slot:item="{item}">
+                    <drag class="draggable" :key="item" :data="item" @cut="removeSkill(mock.skillPool, item)">
+                      <q-badge
+                        class="skill-tag q-mr-xs"
+                        text-color="white"
+                        :color="mySkills.includes(item) ? 'primary' : 'orange'">{{ item }}</q-badge>
+                    </drag>
+                  </template>
+                  <template v-slot:feedback="{data}">
+                    <div class="draggable feedback" :key="data">
+                      <q-badge
+                        class="skill-tag q-mr-xs"
+                        text-color="white"
+                        outline
+                        :color="mySkills.includes(data) ? 'primary' : 'orange'">{{ data }}</q-badge>
+                    </div>
+                  </template>
+                </drop-list>
               </div>
             </div>
           </q-item-label>
@@ -164,12 +195,14 @@ import { Vue, Component, Prop } from 'vue-property-decorator'
 import { JobData } from 'components/models'
 import { formatDescription, getNamedIcon } from 'components/processing'
 import countTo from 'vue-count-to'
-import draggable from 'vuedraggable'
+import { Drag, DropList } from 'vue-easy-dnd'
+import { clone } from 'lodash'
 
 @Component({
   components: {
     countTo, // eslint-disable-line @typescript-eslint/no-unsafe-assignment
-    draggable
+    Drag, // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+    DropList // eslint-disable-line @typescript-eslint/no-unsafe-assignment
   }
 })
 export default class ListingView extends Vue {
@@ -193,8 +226,8 @@ export default class ListingView extends Vue {
   };
 
   mounted () {
-    this.$set(this, 'skillSelected', this.mySkills)
-    this.onDragEnd()
+    this.$set(this, 'skillSelected', clone(this.mySkills))
+    this.onEditSkill()
     this.mock.originalIncome = this.mock.income
   }
 
@@ -211,7 +244,16 @@ export default class ListingView extends Vue {
     this.mock.income = this.mock.originalIncome
   }
 
-  onDragEnd () {
+  onInsertSkill<T> (list: Array<T>, event: {index, data}) {
+    list.splice(event.index, 0, event.data)
+  }
+
+  removeSkill<T> (list: Array<T>, obj: T) {
+    const index = list.indexOf(obj)
+    list.splice(index, 1)
+  }
+
+  onEditSkill () {
     this.lastIncomeVal = this.mock.income
     this.mock.income = this.skillSelected.length * 110 + 90000
   }
@@ -238,26 +280,6 @@ export default class ListingView extends Vue {
   &.brief-view{
     width: 20em;
   }
-}
-
-.multiline-tag {
-  line-height: 1.3em !important;
-}
-
-.skill-tag {
-  //transition: all 300ms;
-  &:hover {
-    //transform: scale(1.02);
-    opacity: 0.9;
-  }
-  cursor: grab;
-  &.moving {
-    cursor: grabbing;
-  }
-}
-
-.select-highlight {
-  background: lighten($primary, 42%);
 }
 
 .income-text {
@@ -287,6 +309,61 @@ export default class ListingView extends Vue {
   }
   to {
     transform: rotate(359deg);
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+.skill-tag {
+  margin-top: 0.2em;
+  &:hover {
+    //transform: scale(1.02);
+    opacity: 0.9;
+  }
+}
+
+.multiline-tag {
+  //line-height: 1.3em !important;
+}
+
+.skillEditing {
+  .draggable-container {
+    padding: 1em;
+    border: 0.5em dashed #c3c3ff66;
+  }
+  .skill-tag {
+    margin-bottom: 0.4em;
+    animation: swaggering 250ms infinite;
+    animation-timing-function: ease-in-out;
+  }
+}
+
+.skill-editor {}
+
+.draggable-container {
+  border: 0 dashed #c3c3ff66;
+  transition: all 700ms;
+}
+
+.draggable {
+  display: inline-flex;
+  flex-direction: column;
+}
+
+$m: 0.3px;
+$q: 1.8deg;
+@keyframes swaggering {
+  0% {
+    transform: rotateZ(0) translateY(0) translateX(0);
+  }
+  33% {
+    transform: rotateZ($q) translateY($m) translateX(-$m);
+  }
+  66% {
+    transform: rotateZ(-$q) translateY(-$m) translateX($m);
+  }
+  100% {
+    transform: rotateZ(0) translateY(0) translateX(0);
   }
 }
 </style>
